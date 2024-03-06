3a3026bc06bb162bffb94d58e9d0af5f7ce45de0
Zachary <37636251+freymend@users.noreply.github.com>
2024-03-05 20:59:17 -0800

    Search API (#9)
    Implement search endpoint

--- server/index.js
+++ server/index.js
@@ -1,12 +1,77 @@
 import Fastify from "fastify";
+import { supabase } from "./db/index.js";
 
 export const app = Fastify({
     logger: true,
 });
 
-app.get("/", async (request, reply) => {
-    return "Hello World";
+await app.register(import("@fastify/swagger"));
+await app.register(import("@fastify/swagger-ui"), {
+    routePrefix: "/",
+    uiConfig: {
+        docExpansion: "full",
+        deepLinking: false,
+    },
+    uiHooks: {
+        onRequest: function (request, reply, next) {
+            next();
+        },
+        preHandler: function (request, reply, next) {
+            next();
+        },
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => {
+        return swaggerObject;
+    },
+    transformSpecificationClone: true,
 });
+
+app.get(
+    "/search/:query",
+    {
+        schema: {
+            params: {
+                query: { type: "string" },
+            },
+            response: {
+                200: {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        properties: {
+                            course_major: { type: "string" },
+                            course_number: { type: "number" },
+                            course_title: { type: "string" },
+                        },
+                    },
+                },
+                500: {
+                    type: "object",
+                    properties: {
+                        message: { type: "string" },
+                    },
+                },
+            },
+            summary: "Search for UW INFO courses.",
+        },
+    },
+    async (request, reply) => {
+        // @ts-ignore
+        const { query } = request.params;
+        const { data, error } = await supabase
+            .from("courses")
+            .select("course_major, course_number, course_title")
+            .textSearch("major_number_title", query);
+        if (error) {
+            reply.status(500);
+            return error;
+        }
+
+        return data;
+    },
+);
 
 try {
     await app.listen({ port: 3000, host: "0.0.0.0" });
