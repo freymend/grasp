14e12609776eb1f3385638752a420ccd48dadeaa
freymond <37636251+freymend@users.noreply.github.com>
2024-03-06 18:20:13 -0800

    use routing

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import Fastify from "fastify";
-import { supabase } from "./db/index.js";
 
 export const app = Fastify({
     logger: true,
@@ -28,51 +27,7 @@ await app.register(import("@fastify/swagger-ui"), {
     transformSpecificationClone: true,
 });
 
-app.get(
-    "/search/:query",
-    {
-        schema: {
-            params: {
-                query: { type: "string" },
-            },
-            response: {
-                200: {
-                    type: "array",
-                    items: {
-                        type: "object",
-                        properties: {
-                            course_major: { type: "string" },
-                            course_number: { type: "number" },
-                            course_title: { type: "string" },
-                        },
-                    },
-                },
-                500: {
-                    type: "object",
-                    properties: {
-                        message: { type: "string" },
-                    },
-                },
-            },
-            summary:
-                "Search for UW INFO courses. The search is case-insensitive and matches any part of the course major, number, or title.",
-        },
-    },
-    async (request, reply) => {
-        // @ts-ignore
-        const { query } = request.params;
-        const { data, error } = await supabase
-            .from("courses")
-            .select("course_major, course_number, course_title")
-            .ilike("major_number_title", `%${query}%`);
-        if (error) {
-            reply.status(500);
-            return error;
-        }
-
-        return data;
-    },
-);
+await app.register(import("./routes/search.js"), { prefix: "/search" });
 
 try {
     await app.listen({ port: 3000, host: "0.0.0.0" });
