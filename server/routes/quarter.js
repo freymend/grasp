7e1178f2e2f7065d4c1a628a8ab5e820f6d034b2
freymond <37636251+freymend@users.noreply.github.com>
2024-04-11 14:08:35 -0700

    expose quarter table

---
+++ server/routes/quarter.js
@@ -0,0 +1,32 @@
+import { supabase } from "../db/index.js";
+
+/**
+ *
+ * @param {import("fastify").FastifyInstance} fastify
+ * @param {Object} _
+ */
+async function routes(fastify, _) {
+    fastify.get(
+        "/",
+        {
+            schema: {
+                response: {
+                    200: { type: "array", items: { type: "string" } },
+                },
+                summary: "Get all the quarters stored in the database",
+            },
+        },
+        async (request, reply) => {
+            const { data, error } = await supabase
+                .from("quarters")
+                .select("quarter");
+            if (error) {
+                reply.status(500);
+                return error;
+            }
+            return data.map((row) => row.quarter);
+        },
+    );
+}
+
+export default routes;
