1b9b221302bfa76b736087854ca426a85cfa2b84
Zachary <37636251+freymend@users.noreply.github.com>
2024-04-18 20:13:35 -0700

    View courses (#28)

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,5 +1,12 @@
 import { supabase } from "../db/index.js";
 
+const quarterOrder = {
+    AUT: 3,
+    WTR: 2,
+    SPR: 1,
+    SUM: 0,
+};
+
 /**
  *
  * @param {import("fastify").FastifyInstance} fastify
@@ -35,6 +42,84 @@ async function routes(fastify, _) {
                 return error;
             }
             return data;
+        },
+    );
+    fastify.get(
+        "/:course_major/:course_number/:course_title",
+        {
+            schema: {
+                params: {
+                    course_major: { type: "string" },
+                    course_number: { type: "string" },
+                    course_title: { type: "string" },
+                },
+                response: {
+                    200: {
+                        type: "object",
+                        properties: {
+                            course_major: { type: "string" },
+                            course_number: { type: "string" },
+                            course_title: { type: "string" },
+                            course_description: { type: "string" },
+                            offered: {
+                                type: "array",
+                                items: {
+                                    type: "object",
+                                    properties: {
+                                        year: { type: "number" },
+                                        quarter: { type: "string" },
+                                        professor: { type: "string" },
+                                        pdf: { type: "string" },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    500: {
+                        type: "object",
+                        properties: {
+                            error: { type: "string" },
+                        },
+                    },
+                },
+                summary:
+                    "Get all years for a specific course. Use this to populate the years filter in the frontend.",
+            },
+        },
+        async (request, reply) => {
+            const course_number = request.params.course_number;
+            const course_major = request.params.course_major.toLowerCase();
+            const course_title = request.params.course_title.toLowerCase();
+
+            const { data, error } = await supabase
+                .from("view_course")
+                .select("*")
+                .eq("course_number", course_number)
+                .eq("course_major", course_major)
+                .eq("course_title", course_title);
+
+            if (error) {
+                reply.status(500);
+                return error;
+            }
+            return {
+                course_major: course_major,
+                course_number: course_number,
+                course_title: course_title,
+                course_description: data[0].course_description,
+                offered: data
+                    .map((row) => ({
+                        year: row.year,
+                        quarter: row.quarter,
+                        professor: `${row.first_name} ${row.last_name}`,
+                        pdf: undefined,
+                    }))
+                    .sort(
+                        (a, b) =>
+                            b.year - a.year ||
+                            quarterOrder[b.quarter] - quarterOrder[a.quarter],
+                    ),
+            };
         },
     );
 }
